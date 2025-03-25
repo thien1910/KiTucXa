3c757f4ac02a54ee547ee696b294b3f92c78f06e
thien1910 <tn6964148@gmail.com>
2025-03-25 22:03:04 +0700

    sửa lỗi thêm RoomService

--- src/main/java/com/project/KiTucXa/Dto/Response/RoomServiceResponse.java
+++ src/main/java/com/project/KiTucXa/Dto/Response/RoomServiceResponse.java
@@ -14,7 +14,7 @@ import java.sql.Timestamp;
 public class RoomServiceResponse {
     String roomServiceId;
     String roomId;
-    Integer utilityServiceId;
+    String utilityServiceId;
     BigDecimal price;
     Timestamp createdAt;
     Timestamp updatedAt;
